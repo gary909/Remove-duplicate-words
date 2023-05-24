a9a9d0223d8a3cc5648dac98ff3c2d02d66c6cb3
gary909 <garywhite61@hotmail.com>
2023-05-24 13:10:33 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 function removeDuplicateWords (s) {
     // your perfect code...
+    var arr = s.split(' ');
+
+    arr = arr.filter(function(value, index, self) { 
+        return self.indexOf(value) === index;
+    }).join(' ');
+
+    return arr;
 }
 
 console.log(removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta')); 
